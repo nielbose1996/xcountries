c8756364043300c2781d79c90d36c3618b61fbfe
Prodipta Bose <nielbose1996@gmail.com>
2024-01-25 21:27:38 +0530

    Test commit

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import App from './App';
 import App2 from './App2';
 import App3 from './FullNameForm';
 import App4 from './Counter'
+import Calculator from './Calculator';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App4 />
+    <Calculator />
   </React.StrictMode>
 );
 
