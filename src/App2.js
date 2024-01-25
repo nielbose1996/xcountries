a3f4aa8ca3ea14546be7c54d7ddd18eb2f63000b
Prodipta Bose <nielbose1996@gmail.com>
2024-01-25 20:14:18 +0530

    Test commit

--- src/App2.js
+++ src/App2.js
@@ -1,6 +1,37 @@
 import React,{useState,useEffect} from 'react';
 
 
-export defaut function App2(){
-    return 0;
+export default function App2(){
+    const[isRunning,setIsRunning]=useState(false);
+    const[elapsedTime,setElapsedTime]=useState(0);
+    const formatTime=(secs)=>{
+        const mins=Math.floor(secs/60);
+        const remainingSecs=secs%60;
+       return `${mins}:${remainingSecs<10?"0":""}${remainingSecs}`;
+    };
+    const toggleTime=()=>{setIsRunning((previousIsRunning)=>!previousIsRunning)};
+    const reset=()=>{
+        setIsRunning(false);
+        setElapsedTime(0);
+        }
+    useEffect(()=>{
+        let intervalId;
+        if(isRunning)
+        {
+            intervalId=setInterval(() => {
+                setElapsedTime((prevElapsedTime)=>prevElapsedTime+1);
+            }, 1000);
+        }
+        else{
+            clearInterval(intervalId);
+        }
+    },[isRunning]);
+    return(
+        <div>
+            <h1>Stopwatch</h1>
+            <p>Time: {formatTime(elapsedTime)}</p>
+             <button onClick={toggleTime}>{isRunning?"Stop":"Start"}</button> 
+             <button onClick={reset}Reset></button>
+        </div>
+    )
 }
